3711f0578f4fd89a7a9a41f1fdfc4730f8ccb7ee
Jacob <>
2023-01-27 12:25:12 +0100

    Scripting 3

--- Mitarbeitskontrolle_Jernej/Assets/Scripts/Player.cs
+++ Mitarbeitskontrolle_Jernej/Assets/Scripts/Player.cs
@@ -18,23 +18,42 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.W))
         {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.position += new Vector3(0, 0, geschwindigkeit * 2);
+            }
+           
             transform.position += new Vector3(0, 0, geschwindigkeit);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.position += new Vector3(-geschwindigkeit * 2,  0, 0);
+            }
+
             transform.position += new Vector3(-geschwindigkeit, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.position += new Vector3(0, 0, -geschwindigkeit * 2);
+            }
+
             transform.position += new Vector3(0, 0, -geschwindigkeit);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.position += new Vector3(geschwindigkeit * 2, 0, 0);
+            }
+
             transform.position += new Vector3(geschwindigkeit, 0, 0);
         }
-
     }
 }
